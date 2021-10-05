829ba50d13dfde8c6b26dde312b06a36e3054aa9
Kennion Gubler <gublerkennion@gmail.com>
2021-10-04 19:14:42 -0600

    Initial Commit

---
+++ Uplft/UplftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UplftApp.swift
+//  Uplft
+//
+//  Created by Kennion Gubler on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct UplftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
