144e826190598cfce49fe3750e882c28142e5ee6
Kennion Gubler <gublerkennion@gmail.com>
2021-10-04 20:58:17 -0600

    Moved Button

--- Uplft/ContentView.swift
+++ Uplft/ContentView.swift
@@ -17,19 +17,20 @@ struct ContentView: View {
             Image("logo")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(minWidth: 50, idealWidth: 50, maxWidth: 100, minHeight: 50, idealHeight: 50, maxHeight: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .padding(.bottom, 20.0)
+                .frame(minWidth: 50, idealWidth: 50, maxWidth: 150, minHeight: 50, idealHeight: 50, maxHeight: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .padding(.bottom, 40.0)
+                .padding(.top, 20.0)
             //Create a Message.
             Text("Create a Message.").font(.system(size: 25, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 0.66, green: 0.66, blue: 0.66, alpha: 1))).tracking(-0.64)
             
             //Enter a positive mes
-            Text("Enter a positive message here.").font(.system(size: 20, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 0.66, green: 0.66, blue: 0.66, alpha: 1))).tracking(-0.51).padding(.top, 75.0).padding(.bottom, 50.0)
+            Text("Enter a positive message here.").font(.system(size: 18, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 0.66, green: 0.66, blue: 0.66, alpha: 1))).tracking(-0.51).padding(.top, 75.0).padding(.bottom, 30.0)
            
                 Section{
                     
                     TextField("You are amazing!", text: $message)
                         .overlay(VStack{Divider().offset(x: 0, y: 15)})
-                        .padding(.top, 100.0).padding(.bottom, 75.0).padding(.leading, 70.0).padding(.trailing, 50.0)
+                        .padding(.top, 50.0).padding(.bottom, 75.0).padding(.leading, 50.0).padding(.trailing, 50.0)
                 }
             Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
                 ZStack
@@ -40,6 +41,7 @@ struct ContentView: View {
                         .shadow(color: Color(#colorLiteral(red: 0.2541666626930237, green: 0.23298610746860504, blue: 0.23298610746860504, alpha: 0.25)), radius:1, x:3, y:3)
                     Text("Send").font(.system(size: 30, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).tracking(-0.64)
                 }
+                .padding(.bottom, 50)
             }
             .padding(.horizontal, 36.0)
         }
